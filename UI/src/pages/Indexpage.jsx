8551ef64ce031a848eb250f4dd308e49e4926c2a
Sneha-p1 <snehaparambadan@gmail.com>
2024-07-11 19:47:17 +0530

    New

---
+++ UI/src/pages/Indexpage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CookBook from '../components/CookBook'
+
+
+
+const Indexpage = () => {
+  return (
+   <>
+   <CookBook/>
+   
+   </>
+  )
+}
+
+export default Indexpage
